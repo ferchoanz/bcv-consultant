8ff3d42e5985fe8dbad2d9b4e492d2f0ff8e4d9b
ferchoanz <fercho0281@gmail.com>
2026-01-22 17:43:26 -0400

    add github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { LoggingInterceptor } from './interceptors/logging-interceptor/logging-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
-    provideHttpClient(),
-  ]
+    provideHttpClient(withInterceptorsFromDi()),
+    { provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true },
+  ],
 };
---
+++ src/app/interceptors/logging-interceptor/logging-interceptor.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class LoggingInterceptor implements HttpInterceptor {
+
+  constructor() {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    console.log(`Request URL: ${request.url}, displayed by the: ` + LoggingInterceptor.name);
+    return next.handle(request);
+  }
+}
