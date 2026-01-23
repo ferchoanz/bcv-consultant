0bf8021d32c664bf821c2159042a68855c1de359
ferchoanz <fercho0281@gmail.com>
2026-01-23 11:25:31 -0400

    feat: environment configurations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://thingproxy.freeboard.io/fetch/https://www.bcv.org.ve'
+  bcvUrl: 'https://thingproxy.freeboard.io/fetch/https://www.bcv.org.ve'
 };
